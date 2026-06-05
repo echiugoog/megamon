b89ffae0ac7ea166352fc328e881d297d42a743b
Edwin Chiu <edwinchiu@google.com>
2026-06-05 15:21:15 -0400

    Fix mock GCS client to be thread safe

--- internal/gcsclient/client_mock.go
+++ internal/gcsclient/client_mock.go
@@ -2,27 +2,57 @@ package gcsclient
 
 import (
 	"context"
+	"sync"
 
 	"example.com/megamon/internal/records"
 )
 
 type mockGCSClient struct {
+	mu      sync.RWMutex
 	records map[string]map[string]records.EventRecords
 }
 
 func (m *mockGCSClient) GetRecords(ctx context.Context, bucket, path string) (map[string]records.EventRecords, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 	rec, ok := m.records[path]
 	if !ok {
 		return map[string]records.EventRecords{}, nil
 	}
-	return rec, nil
+	return cloneEventRecordsMap(rec), nil
 }
 
 func (m *mockGCSClient) PutRecords(ctx context.Context, bucket, path string, recs map[string]records.EventRecords) error {
-	m.records[path] = recs
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.records[path] = cloneEventRecordsMap(recs)
 	return nil
 }
 
 func CreateStubGCSClient() *mockGCSClient {
-	return &mockGCSClient{records: map[string]map[string]records.EventRecords{}}
+	return &mockGCSClient{
+		records: make(map[string]map[string]records.EventRecords),
+	}
+}
+
+func cloneEventRecords(r records.EventRecords) records.EventRecords {
+	if r.UpEvents == nil {
+		return records.EventRecords{}
+	}
+	upEventsCp := make([]records.UpEvent, len(r.UpEvents))
+	copy(upEventsCp, r.UpEvents)
+	return records.EventRecords{
+		UpEvents: upEventsCp,
+	}
+}
+
+func cloneEventRecordsMap(original map[string]records.EventRecords) map[string]records.EventRecords {
+	if original == nil {
+		return nil
+	}
+	cp := make(map[string]records.EventRecords, len(original))
+	for k, v := range original {
+		cp[k] = cloneEventRecords(v)
+	}
+	return cp
 }
--- test/integration/suite_test.go
+++ test/integration/suite_test.go
@@ -24,6 +24,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -235,19 +236,46 @@ func createStubGKEClient() *mockGKEClient {
 }
 
 type mockGCSClient struct {
+	mu      sync.RWMutex
 	records map[string]map[string]records.EventRecords
 }
 
 func (m *mockGCSClient) GetRecords(ctx context.Context, bucket, path string) (map[string]records.EventRecords, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 	rec, ok := m.records[path]
 	if !ok {
 		return map[string]records.EventRecords{}, nil
 	}
-	return rec, nil
+	return cloneEventRecordsMap(rec), nil
 }
 
 func (m *mockGCSClient) PutRecords(ctx context.Context, bucket, path string,
 	recs map[string]records.EventRecords) error {
-	m.records[path] = recs
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.records[path] = cloneEventRecordsMap(recs)
 	return nil
+}
+
+func cloneEventRecords(r records.EventRecords) records.EventRecords {
+	if r.UpEvents == nil {
+		return records.EventRecords{}
+	}
+	upEventsCp := make([]records.UpEvent, len(r.UpEvents))
+	copy(upEventsCp, r.UpEvents)
+	return records.EventRecords{
+		UpEvents: upEventsCp,
+	}
+}
+
+func cloneEventRecordsMap(original map[string]records.EventRecords) map[string]records.EventRecords {
+	if original == nil {
+		return nil
+	}
+	cp := make(map[string]records.EventRecords, len(original))
+	for k, v := range original {
+		cp[k] = cloneEventRecords(v)
+	}
+	return cp
 }
